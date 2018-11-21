09133ba23c4ebaf61fd3ca9e427464d67bd077ed
Julian Wachholz <julian@wachholz.ch>
2018-11-21 10:48:31 +0100

    Only add Select2 translation to media if it exists
    Fixes #1

--- autocompletefilter/admin.py
+++ autocompletefilter/admin.py
@@ -10,18 +10,22 @@ class AutocompleteFilterMixin:
         media = super().media
 
         i18n_file = None
-        i18n_name = SELECT2_TRANSLATIONS.get(get_language())
+        i18n_name = SELECT2_TRANSLATIONS.get(get_language(), None)
         if i18n_name:
             i18n_file = 'admin/js/vendor/select2/i18n/%s.js' % i18n_name
 
-        media._js = OrderedSet([
+        extra_js = [
             'admin/js/vendor/jquery/jquery.js',
             'admin/js/vendor/select2/select2.full.js',
-            i18n_file,
+        ]
+        if i18n_file:
+            extra_js.append(i18n_file)
+        extra_js.extend([
             'admin/js/jquery.init.js',
             'admin/js/autocomplete.js',
             'admin/js/autocomplete_filter.js',
-        ] + media._js)
+        ])
+        media._js = OrderedSet(extra_js + media._js)
         media._css.setdefault('screen', [])
         media._css['screen'].extend([
             'admin/css/vendor/select2/select2.css',
